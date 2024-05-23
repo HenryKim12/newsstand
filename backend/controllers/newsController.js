ce319320d0d8db148ce91353f2a85d4b79b98285
HenryKim12 <henrykim966@gmail.com>
2024-05-23 13:59:27 -0700

    retrieve and store top headlines

--- backend/controllers/newsController.js
+++ backend/controllers/newsController.js
@@ -4,35 +4,46 @@ const URL = "https://newsapi.org/v2/top-headlines?country=us"
 
 // get top headlines
 const getTopHeadlines = async (req, res) => {
-    const options = {
-        method: "GET"
-    }
-    console.log("here")
     try {
-        const response = await fetch(URL + `&apiKey=${process.env.NEWS_API_KEY}`, options)
+        const response = await fetch(URL + `&apiKey=${process.env.NEWS_API_KEY}`, {
+            method: "GET"
+        })
         const data = await response.json()
-        const a1 = data["articles"][0]
-        const obj = {
-            title: a1["title"], 
-            source: a1["source"], 
-            author: a1["author"], 
-            description: a1["description"], 
-            content: a1["content"], 
-            url: a1["url"], 
-            image_url: a1["image_url"],
-            publish_date: a1["publish_date"]
-        }
-        console.log(obj)
-        const article = await Article.create(obj["title"], obj["source"], obj["author"], obj["description"], obj["content"], 
-            obj["url"], obj["image_url"], obj["publish_date"])
-        
-        console.log(article)
-        res.status(200).json(article)
+        const articles = data["articles"]
+        const targets = ["title", "source", "author", "description", "content", "url", "urlToImage", "publishedAt"]
+
+        articles.forEach(async (article) => {
+            let model = {}
+            let invalid = false
+            for (target of targets) {
+                if (article[target]) {
+                    if (target === "source") {
+                        model[target] = article[target]["name"]
+                    } else {
+                        model[target] = article[target]
+                    }
+                } else {
+                    invalid = true
+                    break
+                }
+            }
+
+            if (!invalid) {
+                const title = model["title"]
+                const source = model["source"]
+                const author = model["author"]
+                const description = model["description"]
+                const content = model["content"]
+                const url = model["url"]
+                const image_url = model["urlToImage"]
+                const publish_date = model["publishedAt"]
+                const article_row = await Article.create({title, source, author, description, content, url, image_url, publish_date})
+            }
+        })
+        res.status(200).json({msg: "success"})
     } catch (error) {
         res.status(400).json({error: error.message})
-    }
-    
-    
+    }    
 }
 
 module.exports = {
