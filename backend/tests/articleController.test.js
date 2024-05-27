e9a97f77c90f188cfef5c954ae2d769412d833d8
HenryKim12 <henrykim966@gmail.com>
2024-05-27 16:40:54 -0700

    init article controller tests

---
+++ backend/tests/articleController.test.js
@@ -0,0 +1,19 @@
+const articleController = require("../controllers/articleController")
+
+test("Get all articles", async () => {
+    const res = await articleController.getAllArticles()
+    console.log(res)
+
+})
+
+test("Get article by id", async () => {
+
+})
+
+test("Get article by q", async () => {
+
+})
+
+test("Get article by source", async () => {
+
+})
