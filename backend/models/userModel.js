2e83b8432f34b26bcdb856c5c974230a50f7d95f
HenryKim12 <henrykim966@gmail.com>
2024-06-05 18:36:53 -0700

    added user model with article schema embedded

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose")
+const Article = require("../models/articleModel")
 
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    name:String,
-    username:String,
-    email:String,
-    password:String
+    name: {type: String},
+    username: {type: String},
+    email: {type: String},
+    password: {type: String},
+    articles: [Article.schema]
 })
 
 module.exports = mongoose.model("User", userSchema);
