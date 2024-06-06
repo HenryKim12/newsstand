be08dc4bdaca3df8aad768de58e47835f4ee10b3
HenryKim12 <henrykim966@gmail.com>
2024-06-06 13:52:46 -0700

    draft implementation of user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,9 +3,9 @@ const Article = require("../models/articleModel")
 
 const getUserArticles = async (req, res) => {
     try {
-        let user_id = req.body.id
-        const user = User.findById(user_id)
-        res.status(200).json(user.articles)
+        const user_email = req.body.email
+        const user = await User.findOne({email: user_email})
+        res.status(200).json(user.saved_articles)
     } catch (error) {
         console.log(`Error: ${error.message}`)
         res.status(400).json({error: error.message})
@@ -14,7 +14,21 @@ const getUserArticles = async (req, res) => {
 
 const getUserArticleById = async (req, res) => {
     try {
-
+        const article_id = req.params.id
+        const user_email = req.body.email
+        const user = await User.findOne({email: user_email})
+        const user_articles = user.saved_articles
+        let articleById;
+        for (let article of user_articles) {
+            if (article._id == article_id) {
+                articleById = article._id
+                break
+            }
+        }
+        if (!articleById) {
+            throw new Error(`User does not have article ${article_id} saved`)
+        }
+        res.status(200).json(articleById)
     } catch (error) {
         console.log(`Error: ${error.message}`)
         res.status(400).json({error: error.message})
@@ -23,7 +37,14 @@ const getUserArticleById = async (req, res) => {
 
 const deleteUserArticle = async (req, res) => {
     try {
-
+        const user_email = req.body.email
+        const user = await User.find({email: user_email})
+        const article_id = req.params.id
+        await User.deleteOne(
+            {_id: user_id},
+            {$pull: {saved_articles: {_id: article_id}}}
+        )
+        res.status(204)
     } catch (error) {
         console.log(`Error: ${error.message}`)
         res.status(400).json({error: error.message})
@@ -32,13 +53,16 @@ const deleteUserArticle = async (req, res) => {
 
 const addUserArticle = async (req, res) => {
     try {
-        const user_id = req.body.id
+        const user_email = req.body.email
+        const user = await User.find({email: user_email})
+        const user_id = user._id
         const article_id = req.params.id
         const article = Artcile.findById(article_id)
-        User.updateOne(
+        await User.updateOne(
             {_id: user_id},
             {$push: {saved_articles: article}}
         )
+        res.status(204)
     } catch (error) {
         console.log(`Error: ${error.message}`)
         res.status(400).json({error: error.message})
