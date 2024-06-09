77a16ee10f64b9ed92c51f7107af36252f6f2e19
HenryKim12 <henrykim966@gmail.com>
2024-06-09 16:37:30 -0700

    implemented register api

---
+++ frontend/client-app/src/api/apiClient.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+//const url = import.meta.env.ENV == "dev" ? "" : ""
+
+const apiClient = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+});
+
+export default apiClient
--- frontend/client-app/src/pages/register/Register.jsx
+++ frontend/client-app/src/pages/register/Register.jsx
@@ -1,43 +1,73 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Register.css"
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useNavigate } from 'react-router-dom';
+import apiClient from '../../api/apiClient';
 
 function Register() {
   const navigate = useNavigate()
+  const [formData, setFormData] = useState({
+    name: "", 
+    username: "",
+    email: "",
+    password: ""
+  })
+
+  const registerUser = async (e) => {
+    try {
+      e.preventDefault();
+      const res = await apiClient.post("/api/signup", formData)
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    const {id, value} = e.target;
+    const formType = id.toLowerCase().substring(4)
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [formType]: value
+    }))
+  }
 
   return (
     <div className='main-container'> 
       <h2>Sign up to Newsstand</h2>
-      <Form className='form-container'>
+      <Form className='form-container' onSubmit={registerUser}>
         <Row>
           <Form.Group as={Col} className="mb-3" controlId="formName">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="name" />
+            <Form.Control type="name" onChange={handleChange} />
           </Form.Group>
 
           <Form.Group as={Col} className="mb-3" controlId="formUsername">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="username" />
+            <Form.Control type="username" onChange={handleChange} />
           </Form.Group>
         </Row>
 
         <Form.Group className="mb-3" controlId="formEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" />
+          <Form.Control type="email" onChange={handleChange} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="6+ characters" />
+          <Form.Control type="password" placeholder="6+ characters" onChange={handleChange}/>
         </Form.Group>
-      </Form>
-      <Button variant="primary" type="submit">
+
+        <Button variant="primary" type="submit">
           Create account
-      </Button>
+        </Button>
+      </Form>
+
       <div className='to-login'>
         <p>Already have an account?</p>
         <a href="" onClick={() => navigate("/login")}>Sign In</a>
